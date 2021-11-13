ddc86ad1677ec3e2050a93e62a6653dad0bc44e2
Avazbek Shukurjonov <avazbek.shukurjonov@bk.ru>
2021-11-13 16:01:56 +0500

    Added some code

--- index.py
+++ index.py
@@ -1,2 +1,18 @@
-for letter in 'Elbek':
-    print(letter)
+# for letter in 'Elbek':
+#     print(letter)
+
+# from random import randint
+# print(randint(0,10))
+
+# from random import random
+# print(random()* 100)
+
+# import math
+# from random import random
+# print(math.floor(random()* 100))
+
+# #yoki
+# from random import random
+# from math import floor
+# print(floor(random() * 100))
+
